8eb19a6915679c8a93a6490ab56bc4c00bc47a59
Babatunde Joseph <115642735+Joshluk3328j@users.noreply.github.com>
2025-05-26 07:07:06 +0100

    this file contains my modules

---
+++ my_module.py
@@ -0,0 +1,7 @@
+""""this file contains soe of my modules"""
+def find_sum(num1,num2):
+    """"Returns the sum of two integers"""
+    return num1 + num2
+
+def is_divisible_by2(num):
+    return True if num%2 == 0 else False
